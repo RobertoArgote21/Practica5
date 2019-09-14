9ca28c03b07ab95c1d691b54d158a4bc1a6a779e
LuisAcevedoAscencio <55148344+LuisAcevedoAscencio@users.noreply.github.com>
2019-09-13 20:55:42 -0500

    Add files via upload

---
+++ Ejercicio2 (1).java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Ejercicio2 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+
+		do{
+			int opc,opcion;
+			System.out.println("Ingrese de que figura quiere calcular el area superficial y su volumen");
+			System.out.println(1 +"para prisma");
+			System.out.println(2 + "para circunferencia");
+			System.out.println(3 + "para cono");
+			opc = sc.nextInt();
+			switch(opc){
+				case 1:
+
+					break;
+				case 2:
+
+					break;
+
+				case 3:
+
+					break;
+
+				default:
+
+			}
+
+
+
+
+
+
+
+
+			System.out.println("¿Quiere repetir el programa? Pulse 1");
+
+		}while (opcion == 1);
+
+
+
+	} 
+}
