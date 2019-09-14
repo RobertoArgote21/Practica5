d3bfc7559ff6009c6082fe4bc23574f3f4b3c000
Erick-Leyva <42627970+Erick-Leyva@users.noreply.github.com>
2019-09-13 20:12:36 -0500

    Update Aportacion.java

--- Aportacion.java
+++ Aportacion.java
@@ -1,5 +1,22 @@
-public class Aportacion{
-	public static void main(String args[]){
-	
+import java.util.Scanner;
+public class Esfera{
+	Scanner read=new Scanner(System.in);
+	private float pi=3.1416;
+	public float Area;
+	public float Volumen;
+	public float radio;
+	private Esfera(){	//Constructor
+		Area=0;
+		Volumen=0;
+		System.out.print('Ingrese el radio de la esfera');
+		radio=read.nextFloat();
+	}
+	public float CalcularArea(float r){
+		Area=4*pi*Math.pow(r,2);
+		return Area;
+	}
+	public float CalcularPerimetro(float r){
+		Volumen=(4/3)*pi*Math.pow(r,3);
+		return Volumen;
 	}
 }
