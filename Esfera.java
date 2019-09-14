613ca36275667cccd7860387dd244e17f9a15e7e
Erick-Leyva <42627970+Erick-Leyva@users.noreply.github.com>
2019-09-14 13:56:58 -0500

    Update Esfera.java

--- Esfera.java
+++ Esfera.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 public class Esfera{
 	Scanner read=new Scanner(System.in);
-	public float pi=3.1416f;
+	private final float pi=3.1416f;
 	public float Area;
 	public float Volumen;
-	public float radio;
+	private float radio;
 	private Esfera(){	//Constructor
 		System.out.print("Ingrese el radio de la esfera\t");
 		radio=read.nextFloat();
-		Area=CalcularArea(radio);
 		Volumen=CalcularVolumen(radio);
+		Area=CalcularArea(radio);
+	}
+	public float CalcularVolumen(float r){
+		Volumen=((float)4/3)*(pi)*((float)Math.pow(r,3));
+		return Volumen;
 	}
 	public float CalcularArea(float r){
 		Area=4*pi*((float)Math.pow(r,2));
 		return Area;
 	}
-	public float CalcularVolumen(float r){
-		Volumen=(4/3)*pi*((float)Math.pow(r,3));
-		return Volumen;
-	}
 	public static void main(String[]args){
 	    Esfera esfera1=new Esfera();
-	    System.out.println("Area=" + esfera1.Area + "Volumen=" + esfera1.Volumen);
+	    System.out.println("Area= " + esfera1.Area + "\nVolumen= " + esfera1.Volumen);
 	}
 }
